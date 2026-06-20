4e8ebb70afaf87bc74b26bc8b716119600c0f838
Rajan2810 <Rajanrudhiyal033@gmail.com>
2026-06-20 17:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ TCP_ENDPOINTS = {
     "UP_Emergency": {"ip": "103.195.217.161", "port": 9032},
     "Goa_Emergency": {"ip": "164.100.64.247", "port": 9032},
     "Goa_Primary": {"ip": "164.100.64.226", "port": 9031},
-    "VAHAN NIC": {"ip": "164.100.64.245", "port": 9031}
+  
 }
 
 # HTTP endpoints for manual data sender
