a4a9a59642d8a453b79a15ebb964ce19716c4efc
Rajan2810 <Rajanrudhiyal033@gmail.com>
2026-06-20 17:14:10 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'
    update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ TCP_ENDPOINTS = {
     "UP_primary": {"ip": "103.195.217.160", "port": 9031},
     "UP_Emergency": {"ip": "103.195.217.161", "port": 9032},
     "Goa_Emergency": {"ip": "164.100.64.247", "port": 9032},
-    "Goa_Primary": {"ip": "164.100.64.226", "port": 9031}
+    "Goa_Primary": {"ip": "164.100.64.226", "port": 9031},
     "VAHAN NIC": {"ip": "164.100.64.245", "port": 9031}
 }
 
